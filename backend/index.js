b34339c901f1855c7d1c9d595f8682c860d1b3a2
Dhrumil Bhut <dhrumilbhut@gmail.com>
2023-04-28 19:49:39 +0530

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,21 @@
-const app = require("./app");
+require("dotenv").config();
+const express = require("express");
+const connectToDB = require("./config/db");
+const cors = require("cors");
+
+const todosRoutes = require("./routes/todosRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+connectToDB();
+app.use("/user", userRoutes);
+app.use("/", todosRoutes);
 
 const PORT = process.env.PORT || 3001;
 
